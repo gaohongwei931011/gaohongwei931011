6d842ef25f1b4dc5541ca53733ef4f991f24222c
gaohongwei1993 <1217300470@qq.com>
2021-04-20 11:35:24 +0800

    config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     title: '前端开发手册',
     description: '前端开发手册，打造完善的知识体系',
-    base: '/',
+    base: '/dist/',
+    dest: '/blog/dist',
     themeConfig: {
         lastUpdated: 'Last Updated',
         // smoothScroll: true,
